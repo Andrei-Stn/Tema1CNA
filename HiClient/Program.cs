a36b2a652fbeaef1621f94902fc9d3db96f263c3
Andrei Stan <andystan291@yahoo.com>
2020-04-13 14:32:35 +0300

    First Commit

---
+++ HiClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Grpc.Net.Client;
+using Tema1.Protos;
+
+namespace HiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Hi.HiClient(channel);
+
+            Console.Write("Please enter your name: ");
+
+            var request = new HiRequest();
+            request.Name = Console.ReadLine();
+
+            client.SayHi(request);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema1/Services/HiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Tema1.Protos;
+
+namespace Tema1.Services
+{
+    public class HiService : Hi.HiBase
+    {
+        public override Task<HiReply> SayHi(HiRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Hi " + request.Name);
+            return Task.FromResult(new HiReply());
+        }
+    }
+}
